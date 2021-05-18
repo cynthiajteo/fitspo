a58aa069e7e83087ab751368242b848a4bc29082
cynthiajteo <cynthiajteo@gmail.com>
2021-05-18 22:33:17 +0800

    update views and urls

--- posts/views.py
+++ posts/views.py
@@ -21,8 +21,29 @@ def view_create(request):
                 workout=request.POST['workout'],
                 image=request.FILES['image'])
             post.save()
-            return redirect('/')
+            return redirect('posts:all_posts')
 
     posts = Post.objects.all()
     context = {'form': form}
     return render(request, 'posts/create.html', context)
+
+
+def view_post(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return redirect('posts:all_posts')
+
+    # edit
+    if request.method == 'POST' and request.GET['action'] == 'edit':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:all_posts', post.id)
+
+    if request.GET.get('action') == 'edit':
+        form = PostForm(instance=post)
+        context = {'post': post, 'edit': True, 'form': form}
+        return redirect(request, 'posts/show.html', context)
+    
+    

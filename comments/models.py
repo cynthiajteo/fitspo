796570ecfc453253b11d1951345262d7ed706ec6
cynthiajteo <cynthiajteo@gmail.com>
2021-05-20 21:16:21 +0800

    comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+import uuid
+from posts.models import *
+from accounts.models import User
 # Create your models here.
+
+
+class Comments(models.Model):
+    id = models.UUIDField(  # new
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    comment = models.CharField(max_length=500, null=False)
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE, related_name='comments')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user': {
+                'username': self.name.username
+            },
+            'comment': self.comment,
+            'post': {
+                'image': self.post.image,
+                'workout': self.post.workout
+            }
+        }

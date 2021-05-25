06cb074cf300e2f614ee4ed2414a8cffb5c0a525
cynthiajteo <cynthiajteo@gmail.com>
2021-05-25 19:38:59 +0800

    shifted edit form to own page

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,6 @@ app_name = "posts"
 urlpatterns = [
     path('', view_index, name='all_posts'),
     path('create/', view_create, name='post_create'),
-    path('post<uuid:pk>', view_post, name='post_show'),
+    path('edit<uuid:pk>/', view_post, name='post_edit'),
+    path('post<uuid:pk>', view_show_post, name='post_show'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -34,7 +34,7 @@ def view_create(request):
     return render(request, 'posts/create.html', context)
 
 
-# edit and delete post in show page
+# edit form
 @login_required
 def view_post(request, pk):
     try:
@@ -51,17 +51,48 @@ def view_post(request, pk):
 
             if form.is_valid():
                 form.save()
-                return redirect('posts:post_show', post.id)
+                return redirect('posts:post_edit', post.id)
 
         if request.GET.get('action') == 'edit':
             form = EditForm(instance=post)
 
             context = {'post': post, 'edit': True, 'form': form}
-            return render(request, 'posts/show.html', context)
+            return render(request, 'posts/edit.html', context)
     else:
         context = {"post": post, "edit": False}
         return render(request, 'posts/others.html', context)
 
+    context = {"post": post, "edit": False}
+    return render(request, 'posts/edit.html', context)
+
+
+# show post
+@login_required
+def view_show_post(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return redirect('posts:all_posts')
+    if request.user == post.name:
+        if request.GET.get('action') == 'del':
+            post.delete()
+            return redirect('posts:all_posts')
+
+        if request.method == 'POST' and request.GET['action'] == 'edit':
+            form = EditForm(request.POST, request.FILES, instance=post)
+
+            if form.is_valid():
+                form.save()
+                return redirect('posts:post_edit', post.id)
+
+        if request.GET.get('action') == 'edit':
+            form = EditForm(instance=post)
+
+            context = {'post': post, 'edit': True, 'form': form}
+            return render(request, 'posts/edit.html', context)
+    else:
+        context = {"post": post, "edit": False}
+        return render(request, 'posts/others.html', context)
     context = {"post": post, "edit": False}
     return render(request, 'posts/show.html', context)
 
@@ -74,5 +105,3 @@ def view_like(request, pk):
     like.liked = request.POST['liked'] == 'true'
     like.save()
     return render(request, 'posts:all_posts')
-
-
